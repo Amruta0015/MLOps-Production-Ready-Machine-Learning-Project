a028c515dc9c74d3732cff9279f95e2238bff5e0
Amruta0015 <amrutapatil0015@gmail.com>
2025-02-19 14:52:12 +0530

    readme file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Amruta",
+    author_email="amrutapatil0015@gmail.com",
+    packages=find_packages()
+)
